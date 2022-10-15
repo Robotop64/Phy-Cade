ebf4eb7b4c7c1337e6bdcbccf144059ee427eb75
Maximilian Aleksandrov <maximilian.aleksandrov@uni.kn>
2022-10-16 00:51:54 +0200

    * InputListener.java:   + support for inferior input devices
    * stuff

--- src/Gui.java
+++ src/Gui.java
@@ -1,82 +1,84 @@
-import net.java.games.input.Controller;
-
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 import java.awt.Color;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 
-public class Gui {
+public class Gui
+{
 
-    public static final int frame_width = 1280;
-    public static final int frame_height = 1024;
-    private static Gui instance;
+  public static final int frame_width  = 1280;
+  public static final int frame_height = 1024;
+  private static      Gui instance;
 
-    JFrame frame;
+  JFrame frame;
 
-    private Gui(){
+  private Gui ()
+  {
 
-    };
+  }
 
-    public void initialize(){
-        frame = new JFrame("Pac-Φ");
-        frame.setSize(frame_width, frame_height);
-        frame.setLocationRelativeTo(null);
-        frame.setLocation(-frame_width, 0);
+  ;
 
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setUndecorated(true);
-        frame.getContentPane().setBackground(Color.black);
-        frame.getContentPane().setLayout(null);
+  public void initialize ()
+  {
+    frame = new JFrame("Pac-Φ");
+    frame.setSize(frame_width, frame_height);
+    frame.setLocationRelativeTo(null);
+    frame.setLocation(-frame_width, 0);
 
-        frame.getContentPane().add(new MainMenu());
+    frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    frame.setUndecorated(true);
+    frame.getContentPane().setBackground(Color.black);
+    frame.getContentPane().setLayout(null);
 
-        frame.setVisible(true);
+    InputListener inputListener = InputListener.getInstance();
+    inputListener.start();
 
-        InputListener inputListener = InputListener.getInstance();
-        inputListener.start();
+    inputListener.subscribe(System.out::println);
 
-        inputListener.subscribe(System.out::println);
-
-
-        MainMenu mainMenu = new MainMenu();
-        frame.getContentPane().add(mainMenu);
-        mainMenu.setBounds(0, 0, frame_width, frame_height);
-
-
-        pmButton debug = new pmButton("");
-        debug.setForeground(Color.red);
-        debug.setBorder(null);
-        debug.setFontSize(14);
-        frame.getContentPane().add(debug);
-        debug.setBounds(1, frame_height - 55, 400, 40);
-        inputListener.subscribe(input -> debug.setText(input.toString()));
+    frame.getContentPane().add(MainMenu.getInstance());
+    MainMenu.getInstance().setBounds(0, 0, frame_width, frame_height);
 
 
-        frame.getContentPane().setComponentZOrder(debug, 0);
-        frame.getContentPane().setComponentZOrder(mainMenu, 1);
+    pmButton debug = new pmButton("");
+    debug.setForeground(Color.red);
+    debug.setBorder(null);
+    debug.setFontSize(14);
+    frame.getContentPane().add(debug);
+    debug.setBounds(1, frame_height - 55, 400, 40);
+    inputListener.subscribe(input -> debug.setText(input.toString()));
 
-        pos();
 
-        frame.setVisible(true);
+    frame.getContentPane().setComponentZOrder(debug, 0);
+    frame.getContentPane().setComponentZOrder(MainMenu.getInstance(), 1);
+
+    //    pos();
+
+    frame.setVisible(true);
+  }
+
+  private void pos ()
+  {
+    frame.setUndecorated(false);
+    frame.addComponentListener(new ComponentAdapter()
+    {
+      @Override
+      public void componentMoved (ComponentEvent e)
+      {
+        super.componentMoved(e);
+        System.out.println(e);
+      }
+    });
+  }
+
+  public static Gui getInstance ()
+  {
+    if (instance == null)
+    {
+      instance = new Gui();
     }
 
-    private void pos(){
-        frame.setUndecorated(false);
-        frame.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentMoved(ComponentEvent e) {
-                super.componentMoved(e);
-                System.out.println(e);
-            }
-        });
-    }
-
-    public static Gui getInstance() {
-        if (instance == null) {
-            instance = new Gui();
-        }
-
-        return instance;
-    }
+    return instance;
+  }
 }
