f2262cf6daf4be4cb94c03619260169c8d31ad6d
Maximilian Aleksandrov <maximilian.aleksandrov@uni.kn>
2022-10-17 18:27:15 +0200

    * Celebrity   + logging

--- src/Celebrity.java
+++ src/Celebrity.java
@@ -1,26 +1,37 @@
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-public abstract class Celebrity<Type> {
+public abstract class Celebrity <Type>
+{
 
-    private static int subId = 0;
-    ConcurrentMap<Integer, Fan<Type>> subscribers = new ConcurrentHashMap<>();
+  private static int subId = 0;
+  ConcurrentMap <Integer, Fan <Type>> subscribers = new ConcurrentHashMap <>();
 
-    public void post(Type type){
-        subscribers.forEach((id, fan) -> fan.handle(type));
-    }
+  public void post (Type type)
+  {
+    subscribers.forEach((id, fan) -> fan.handle(type));
+  }
 
-    public int subscribe(Fan<Type> fan){
-        subscribers.put(subId, fan);
-        return subId++;
-    }
+  public int subscribe (Fan <Type> fan)
+  {
+    int id = subId;
+    subId++;
+    subscribers.put(id, fan);
+    System.out.println(id + " subbed");
+    System.out.println("all subs: " + subscribers);
+    return id;
+  }
 
-    public void unsubscribe(int id){
-        subscribers.remove(id);
-    }
+  public void unsubscribe (int id)
+  {
+    subscribers.remove(id);
+    System.out.println(id + " unsubbed");
+    System.out.println("all subs: " + subscribers);
+  }
 
-    interface Fan<Type>{
-        void handle(Type type);
-    }
+  interface Fan <Type>
+  {
+    void handle (Type type);
+  }
 
 }
