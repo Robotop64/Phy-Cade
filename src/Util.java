0b143e04f51ce5ef455b7f2c25fffa71c0d6b8de
David Capalbo <david.capalbo@uni.kn>
2022-10-19 00:28:18 +0200

    * Util   + ptToPx   + pxToPt

--- src/Util.java
+++ src/Util.java
@@ -3,4 +3,7 @@ public class Util
 
   public static int bounded (int x, int min, int max) { return Math.max(Math.min(x, max), min); }
 
+  public static int ptToPx (int pt) { return (int) ( pt * 1.333 ); }
+
+  public static int pxToPt (int px) { return (int) ( px / 1.333 ); }
 }
