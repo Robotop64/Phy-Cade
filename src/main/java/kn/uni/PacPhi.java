42c52a7d2b7d1bbb78ed307c98c711b0ef09e6e8
Maximilian Aleksandrov <maximilian.aleksandrov@uni-konstanz.de>
2022-10-28 02:31:03 +0200

    fixed gitignore added Celebrity.java

---
+++ src/main/java/kn/uni/PacPhi.java
@@ -0,0 +1,9 @@
+package kn.uni;
+
+public class PacPhi
+{
+  public static void main (String[] args)
+  {
+    System.out.println("test");
+  }
+}
---
+++ src/main/java/kn/uni/ui/Celebrity.java
@@ -0,0 +1,53 @@
+package kn.uni.ui;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public abstract class Celebrity <Type>
+{
+
+  private static int subId = 0;
+  ConcurrentMap <Integer, Fan <Type>> subscribersQueue = new ConcurrentHashMap <>();
+  ConcurrentMap <Integer, Fan <Type>> subscribers      = new ConcurrentHashMap <>();
+
+  boolean locked = false;
+
+  public void post (Type type)
+  {
+    locked = true;
+    subscribers.forEach((id, fan) -> fan.handle(type));
+    locked = false;
+    subscribers.putAll(subscribersQueue);
+    subscribersQueue.clear();
+  }
+
+  public int subscribe (Fan <Type> fan)
+  {
+    int id = subId;
+    subId++;
+    subscribersQueue.put(id, fan);
+    System.out.println(id + " wants to sub");
+    System.out.println("all subs: " + subscribers);
+    System.out.println("sub queue: " + subscribersQueue);
+
+    if (!locked)
+    {
+      subscribers.putAll(subscribersQueue);
+      subscribersQueue.clear();
+    }
+    return id;
+  }
+
+  public void unsubscribe (int id)
+  {
+    subscribers.remove(id);
+    System.out.println(id + " unsubbed");
+    System.out.println("all subs: " + subscribers);
+  }
+
+  interface Fan <Type>
+  {
+    void handle (Type type);
+  }
+
+}
