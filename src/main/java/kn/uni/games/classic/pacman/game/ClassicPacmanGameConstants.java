4a6f8b6246eab8a212f7b826774bf85518eefa1e
David Capalbo <david.capalbo@uni.kn>
2022-11-12 17:23:13 +0100

    moved collectionPoints map to ClassicPacmanGameConstants

--- src/main/java/kn/uni/games/classic/pacman/game/ClassicPacmanGameConstants.java
+++ src/main/java/kn/uni/games/classic/pacman/game/ClassicPacmanGameConstants.java
@@ -1,7 +1,23 @@
 package kn.uni.games.classic.pacman.game;
 
+import java.util.Map;
+
 public class ClassicPacmanGameConstants
 {
   public static final double pacmanSpeed = 6;
   public static final double ghostRadius = 31;
+
+  //how many points a fruit gives
+  public static final Map <ClassicPacmanGameState.Collectables, Integer> collectionPoints = Map.of(
+      ClassicPacmanGameState.Collectables.coin, 10,
+      ClassicPacmanGameState.Collectables.powerUp, 50,
+      ClassicPacmanGameState.Collectables.cherry, 100,
+      ClassicPacmanGameState.Collectables.strawberry, 300,
+      ClassicPacmanGameState.Collectables.orange, 500,
+      ClassicPacmanGameState.Collectables.apple, 700,
+      ClassicPacmanGameState.Collectables.melon, 1000,
+      ClassicPacmanGameState.Collectables.galaxian, 2000,
+      ClassicPacmanGameState.Collectables.bell, 3000,
+      ClassicPacmanGameState.Collectables.key, 5000
+  );
 }
--- src/main/java/kn/uni/games/classic/pacman/game/ClassicPacmanGameState.java
+++ src/main/java/kn/uni/games/classic/pacman/game/ClassicPacmanGameState.java
@@ -10,56 +10,44 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 
 public class ClassicPacmanGameState
 {
-	//frames will be drawn every second frame
-	public int                                tps             = 120;
-	public ConcurrentLinkedDeque <GameObject> gameObjects;
-	public boolean                            running;
-	public long                               currentTick;
-	public long                               lastTickTime;
-	public Direction                          playerDirection = Direction.down;
-	public boolean                            logging;
-	public Vector2d                           size;
-	public ClassicPacmanMap                   map;
-	public int                                mapOffset       = 15;
+  //frames will be drawn every second frame
+  public int                                tps             = 120;
+  public ConcurrentLinkedDeque <GameObject> gameObjects;
+  public boolean                            running;
+  public long                               currentTick;
+  public long                               lastTickTime;
+  public Direction                          playerDirection = Direction.down;
+  public boolean                            logging;
+  public Vector2d                           size;
+  public ClassicPacmanMap                   map;
+  public int                                mapOffset       = 15;
 
-	//score and counter stuff
-	public long      score        = 0;
-	public int       level        = 1;
-	public int       uiSize       = 100;
-	public int       lives        = 5;
-	public LocalTime startTime;
-	public LocalTime gameDuration = LocalTime.of(0, 0, 0, 0);
-	public int       eatenPills   = 0;
-	public int       pillsLeft    = 244;
+  //score and counter stuff
+  public long      score        = 0;
+  public int       level        = 1;
+  public int       uiSize       = 100;
+  public int       lives        = 5;
+  public LocalTime startTime;
+  public LocalTime gameDuration = LocalTime.of(0, 0, 0, 0);
+  public int       eatenPills   = 0;
+  public int       pillsLeft    = 244;
 
 
-	// ToDo move this to a better place (has nothing to do with the game state) ~Max
-	//how many points a fruit gives
-	public Map <Collectables, Integer> collectionPoints = Map.of(
-		Collectables.coin, 10,
-		Collectables.powerUp, 50,
-		Collectables.cherry, 100,
-		Collectables.strawberry, 300,
-		Collectables.orange, 500,
-		Collectables.apple, 700,
-		Collectables.melon, 1000,
-		Collectables.galaxian, 2000,
-		Collectables.bell, 3000,
-		Collectables.key, 5000
-	);
+  // ToDo move this to a better place (has nothing to do with the game state) ~Max
 
-	// @David Do we need this? and if we need it, do we need it here? ~Max
-	// how many items were collected
-	public Map <Collectables, Integer> collectionCount;
 
-	//objects added to game
-	public ClassicPacmanGameState ()
-	{
-		gameObjects = new ConcurrentLinkedDeque <>();
-	}
+  // @David Do we need this? and if we need it, do we need it here? ~Max
+  // how many items were collected
+  public Map <Collectables, Integer> collectionCount;
 
-	// ToDo move this to a better place (has nothing to do with the game state) (same as above) ~Max
-	//variety of fruits
-	public enum Collectables
-	{ coin, powerUp, cherry, strawberry, apple, orange, melon, galaxian, bell, key }
+  //objects added to game
+  public ClassicPacmanGameState ()
+  {
+    gameObjects = new ConcurrentLinkedDeque <>();
+  }
+
+  // ToDo move this to a better place (has nothing to do with the game state) (same as above) ~Max
+  //variety of fruits
+  public enum Collectables
+  { coin, powerUp, cherry, strawberry, apple, orange, melon, galaxian, bell, key }
 }
