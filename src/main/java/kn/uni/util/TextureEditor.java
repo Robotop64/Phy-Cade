d0ed75ffbdbe53a2d03b4ebe58cd18a12d4cb4f7
David Capalbo <david.capalbo@uni.kn>
2022-11-26 15:59:46 +0100

    merged loading, saving and editing of textures into one singleton

---
+++ src/main/java/kn/uni/util/TextureEditor.java
@@ -0,0 +1,78 @@
+package kn.uni.util;
+
+import javax.imageio.ImageIO;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class TextureEditor
+{
+  private static final String texturePath = "pacman/textures";
+
+  //singleton pattern
+  private static final TextureEditor instance = new TextureEditor();
+
+  private TextureEditor () { }
+
+  public static TextureEditor getInstance ()
+  {
+    return instance;
+  }
+
+  public void saveImage (BufferedImage imageIn, String path, String type) throws IOException
+  {
+    String source = "src/main/resources/";
+    ImageIO.write(imageIn, type, new File(source + texturePath + path + "." + type.toLowerCase()));
+  }
+
+  public BufferedImage loadTexture (String category, String name)
+  {
+    try
+    {
+      return ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream(texturePath + "/" + category + "/" + name)));
+    }
+    catch (IOException e)
+    {
+      e.printStackTrace();
+      System.out.println("failed to load texture " + texturePath + "/" + category + "/" + name);
+    }
+    return null;
+  }
+
+  public BufferedImage createOutline (BufferedImage imageIn, int thickness, Color c)
+  {
+    //create outputImage
+    BufferedImage output = new BufferedImage(imageIn.getWidth(), imageIn.getHeight(), BufferedImage.TYPE_INT_ARGB);
+    //for each pixelRow in the image
+    IntStream.range(0, imageIn.getWidth() - 1).forEach(xPixel ->
+    {
+      //for each pixelColumn in the image
+      IntStream.range(0, imageIn.getHeight() - 1).forEach(yPixel ->
+      {
+        //color of evaluated pixel
+        Color currentPixel = new Color(imageIn.getRGB(xPixel, yPixel), true);
+        //filter for only border pixels
+        if (currentPixel.getAlpha() == 0)
+        {
+          //check if pixel has texture neighbors
+          for (int φ = 0; φ < 360; φ += 90)
+          {
+            Vector2d v              = new Vector2d().polar(1, φ);
+            int      nextX          = Util.bounded((int) ( xPixel + v.x ), 0, imageIn.getWidth());
+            int      nextY          = Util.bounded((int) ( yPixel + v.y ), 0, imageIn.getHeight());
+            Color    neighbourPixel = new Color(imageIn.getRGB(nextX, nextY), true);
+            //if neighbor is not transparent, set pixel to outline color
+            if (neighbourPixel.getAlpha() != 0)
+            {
+              output.setRGB(xPixel, yPixel, c.getRGB());
+            }
+          }
+        }
+      });
+    });
+    return output;
+  }
+}
