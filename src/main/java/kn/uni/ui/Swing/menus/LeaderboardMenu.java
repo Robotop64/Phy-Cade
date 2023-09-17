95c1c66197a2c5b5b5b7c577feb69ed745d8ef9a
David Capalbo <david.capalbo@uni-konstanz.de>
2023-09-17 12:46:28 +0200

    refactor for readability

--- src/main/java/kn/uni/ui/Swing/menus/LeaderboardMenu.java
+++ src/main/java/kn/uni/ui/Swing/menus/LeaderboardMenu.java
@@ -1,13 +1,12 @@
 package kn.uni.ui.Swing.menus;
 
 import kn.uni.Gui;
+import kn.uni.games.classic.pacman.persistence.PacmanDatabaseProvider;
 import kn.uni.ui.InputListener;
 import kn.uni.ui.Swing.Style;
 import kn.uni.ui.Swing.components.PacComboBox;
 import kn.uni.ui.Swing.components.PacLabel;
-import kn.uni.ui.Swing.components.PacTree;
 import kn.uni.ui.Swing.components.RoundedPanel;
-import kn.uni.ui.Swing.components.SettingEditor;
 import kn.uni.ui.Swing.interfaces.Controllable;
 import kn.uni.ui.UIScreen;
 import kn.uni.util.Vector2d;
@@ -17,22 +16,23 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.event.ItemEvent;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 public class LeaderboardMenu extends UIScreen implements Controllable
 {
 
   public static LeaderboardMenu instance;
-  Config.Tree currentSettingGroup;
-  public JPanel      editors;
-  public JScrollPane scroll;
+  public String[] dbFields;
+  public String[] tableFields;
+  private final int entryCount = 15;
 
   public LeaderboardMenu (JPanel parent)
   {
@@ -41,7 +41,11 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     setBackground(Color.BLACK);
     setVisible(true);
 
+    dbFields = new String[4];
+
     addComponents();
+
+    refreshTable(0);
   }
 
   public static LeaderboardMenu getInstance (JPanel parent)
@@ -53,17 +57,50 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     return instance;
   }
 
+  private int buffer     = 20;
+  private int lineHeight = 60;
+  private int lineWeight = 3;
+  private int widgetArc  = 30;
+  private int cBuffer    = buffer * 2 / 3;
+  private int vIBuffer   = 10;
+
+  ComponentSpec title;
+  ComponentSpec info;
+  ComponentSpec gameInfo;
+  ComponentSpec table;
+
   public void addComponents ()
   {
-    int buffer     = 20;
-    int lineHeight = 60;
-    int lineWeight = 3;
-    int widgetArc  = 30;
+    title = makeTitle();
 
-    //region title
+    info = makeInfo();
+
+    gameInfo = makeGameInfo();
+
+    table = makeTable();
+
+//    IntStream.range(0, entryCount).forEach(i -> {
+//      JPanel entry = new JPanel();
+//      entry.setMaximumSize(new Dimension(Short.MAX_VALUE, Short.MAX_VALUE));
+////      entry.setLayout(new BorderLayout());
+//      entry.setBackground(Color.RED);
+//      entry.setForeground(null);
+//      eContent.add(entry);
+//
+//      if (i < entryCount - 1)
+//        eContent.add(Box.createRigidArea(new Dimension(0, vIBuffer)));
+//    });
+  }
+
+  record ComponentSpec (Vector2d pos, Dimension size, Component comp) { }
+
+  private ComponentSpec makeTitle()
+  {
     Dimension titleSize = new Dimension(500, lineHeight);
+    Vector2d  titlePos  = new Vector2d().cartesian(Gui.frameWidth / 2. - titleSize.width / 2., buffer);
+
     PacLabel title = new PacLabel(
-        new Vector2d().cartesian(Gui.frameWidth / 2. - titleSize.width / 2., buffer),
+        titlePos,
         titleSize,
         ">>- Leaderboard -<<");
     title.setFont(title.getFont().deriveFont(40f));
@@ -71,11 +108,15 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     title.useColorSet(Style.normal);
     title.setHorizontalAlignment(PacLabel.CENTER);
     add(title);
-    //endregion
 
+    return new ComponentSpec(titlePos, titleSize, title);
+  }
+
+  private ComponentSpec makeInfo()
+  {
     Dimension infoSize = new Dimension(Gui.frameWidth - 2 * buffer, (int) ( 2 * lineHeight * 0.75 + buffer ));
     Vector2d  infoPos  = new Vector2d().cartesian(buffer, Gui.frameHeight - infoSize.height - buffer);
-    //region info panel
+
     RoundedPanel info = new RoundedPanel();
     info.setLocation((int) infoPos.x, (int) infoPos.y);
     info.setSize(infoSize);
@@ -83,13 +124,16 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     info.setBorderWidth(lineWeight);
     info.useColorSet(Style.normal);
     add(info);
-    //endregion
 
+    return new ComponentSpec(infoPos, infoSize, info);
+  }
 
-    Vector2d  gameInfoPos  = new Vector2d().cartesian(buffer, title.getLocation().y + title.getHeight() + buffer);
-    Dimension gameInfoSize = new Dimension(350, (int) ( Gui.frameHeight - gameInfoPos.y - infoSize.height - 2 * buffer ));
+  private ComponentSpec makeGameInfo()
+  {
+    Vector2d  gameInfoPos  = new Vector2d().cartesian(buffer, title.pos().y + title.size().getHeight() + buffer);
+    Dimension gameInfoSize = new Dimension(350, (int) ( Gui.frameHeight - gameInfoPos.y - info.size().height - 2 * buffer ));
 
-    //region game info panel
+    //region parent
     RoundedPanel gameInfo = new RoundedPanel();
     gameInfo.setLocation((int) gameInfoPos.x, (int) gameInfoPos.y);
     gameInfo.setSize(gameInfoSize);
@@ -98,18 +142,16 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     gameInfo.useColorSet(Style.normal);
     gameInfo.setLayout(null);
     add(gameInfo);
+    //endregion
 
-    //region game info content
+    //region children
     JPanel gameInfoContent = new JPanel();
-    int    cBuffer         = buffer * 2 / 3;
     gameInfoContent.setLocation(cBuffer, cBuffer);
     gameInfoContent.setSize(gameInfoSize.width - 2 * cBuffer, gameInfoSize.height - 2 * cBuffer);
     gameInfoContent.setLayout(new BoxLayout(gameInfoContent, BoxLayout.Y_AXIS));
     gameInfoContent.setBackground(null);
     gameInfoContent.setForeground(null);
     gameInfo.add(gameInfoContent);
-    int vIBuffer = 10;
-
 
     //region game selector title
     JPanel gST = new JPanel();
@@ -132,7 +174,16 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     renderer.setHorizontalAlignment(JLabel.CENTER);
     gameSelector.setFont(renderer.getFont().deriveFont(20f));
 
+    gameSelector.addItemListener(e -> {
+      if (e.getStateChange() == ItemEvent.SELECTED) {
+        dbFields[0] = Objects.requireNonNull(gameSelector.getSelectedItem()).toString();
+        refreshTable(0);
+      }
+    });
+
     gameSelector.setSelectedItem(gameSetting.toValue().current);
+    dbFields[0] = gameSetting.toValue().current;
+
     gS.add(gameSelector, BorderLayout.CENTER);
     gameInfoContent.add(gS);
     //endregion
@@ -160,7 +211,16 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     renderer.setHorizontalAlignment(JLabel.CENTER);
     modeSelector.setFont(renderer.getFont().deriveFont(20f));
 
+    modeSelector.addItemListener(e -> {
+      if (e.getStateChange() == ItemEvent.SELECTED) {
+        dbFields[1] = Objects.requireNonNull(modeSelector.getSelectedItem()).toString();
+        refreshTable(0);
+      }
+    });
+
     modeSelector.setSelectedItem(modeSetting.toValue().current);
+    dbFields[1] = modeSetting.toValue().current;
+
     mS.add(modeSelector, BorderLayout.CENTER);
     gameInfoContent.add(mS);
     //endregion
@@ -180,8 +240,10 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     //region sort selector
     JPanel sS = new JPanel();
     sS.setMaximumSize(new Dimension(Short.MAX_VALUE, lineHeight));
-    sS.setLayout(new BorderLayout());
+    sS.setLayout(new BoxLayout(sS, BoxLayout.X_AXIS));
+    sS.setBackground(null);
 
+    //region sort by
     Config.Setting       sortSetting  = Config.getInstance().getSetting("General/Leaderboard/SortBy");
     List <String>        sorts        = new ArrayList <>(List.of(sortSetting.toValue().possible));
     if (Config.getInstance().hasSetting("Gameplay/" + gameSetting.toValue().current + "/Leaderboard/SortBy"))
@@ -190,7 +252,7 @@ public class LeaderboardMenu extends UIScreen implements Controllable
       sorts.addAll(List.of(gameSortSetting.toValue().possible));
     }
 
-    PacComboBox <String> sortSelector = new PacComboBox <>(sortSetting.toValue().possible);
+    PacComboBox <String> sortSelector = new PacComboBox <>(sorts.toArray(new String[0]));
     sortSelector.setEditable(true);
 
     renderer = (JLabel) sortSelector.getRenderer();
@@ -199,100 +261,98 @@ public class LeaderboardMenu extends UIScreen implements Controllable
     tF.setHorizontalAlignment(JTextField.CENTER);
     sortSelector.setFont(tF.getFont().deriveFont(20f));
 
+    sortSelector.addItemListener(e -> {
+      if (e.getStateChange() == ItemEvent.SELECTED) {
+        dbFields[2] = Objects.requireNonNull(sortSelector.getSelectedItem()).toString();
+        refreshTable(0);
+      }
+    });
+
     sortSelector.setSelectedItem(sortSetting.toValue().current);
-    sS.add(sortSelector, BorderLayout.CENTER);
+    dbFields[2] = sortSetting.toValue().current;
+
+    sS.add(sortSelector);
+    //endregion
+
+    sS.add(Box.createRigidArea(new Dimension(vIBuffer, 0)));
+
+    //region sort direction
+    PacComboBox<String> sortDir = new PacComboBox<>(new String[]{"Desc", "Asc"});
+    sortDir.setEditable(true);
+
+    renderer = (JLabel) sortDir.getRenderer();
+    renderer.setHorizontalAlignment(JLabel.CENTER);
+    tF = (JTextField) sortDir.getEditor().getEditorComponent();
+    tF.setHorizontalAlignment(JTextField.CENTER);
+    sortDir.setFont(tF.getFont().deriveFont(20f));
+
+    sortDir.addItemListener(e -> {
+      if (e.getStateChange() == ItemEvent.SELECTED) {
+        dbFields[3] = Objects.requireNonNull(sortDir.getSelectedItem()).toString();
+        refreshTable(0);
+      }
+    });
+
+    sortDir.setSelectedItem("Desc");
+    dbFields[3] = "Desc";
+
+    sS.add(sortDir);
+
+    //endregion
+
     gameInfoContent.add(sS);
+
     //endregion
 
     gameInfoContent.revalidate();
+    return new ComponentSpec(gameInfoPos, gameInfoSize, gameInfo);
+  }
+
+  private ComponentSpec makeTable()
+  {
+    Vector2d  tablePos  = new Vector2d().cartesian(gameInfo.pos().x + gameInfo.size().getWidth() + buffer, gameInfo.pos().y);
+    Dimension tableSize = new Dimension((int) ( Gui.frameWidth - tablePos.x - buffer ), gameInfo.size().height);
+
+    RoundedPanel table = new RoundedPanel();
+    table.setLocation((int) tablePos.x, (int) tablePos.y);
+    table.setSize(tableSize);
+    table.setArc(widgetArc);
+    table.setBorderWidth(lineWeight);
+    table.useColorSet(Style.normal);
+    table.setLayout(null);
+    add(table);
+
+    //region table header
+    JPanel tHeader = new JPanel();
+    tHeader.setLocation(cBuffer, cBuffer);
+    tHeader.setSize(tableSize.width - 2 * cBuffer, lineHeight);
+    tHeader.setLayout(new BoxLayout(tHeader, BoxLayout.X_AXIS));
+    tHeader.setBackground(Color.BLUE);
+    tHeader.setForeground(null);
+    table.add(tHeader);
     //endregion
 
+    //region table content
+    JPanel tContent = new JPanel();
+    tContent.setLocation(cBuffer, tHeader.getLocation().y+tHeader.getHeight() + cBuffer);
+    tContent.setSize(tableSize.width - 2 * cBuffer, tableSize.height - tHeader.getHeight() - 3 * cBuffer);
+    tContent.setLayout(new BoxLayout(tContent, BoxLayout.Y_AXIS));
+    tContent.setBackground(Color.RED);
+    tContent.setForeground(null);
+    table.add(tContent);
     //endregion
 
-
-    Vector2d  editorPos  = new Vector2d().cartesian(gameInfo.getLocation().x + gameInfo.getWidth() + buffer, gameInfo.getLocation().y);
-    Dimension editorSize = new Dimension((int) ( Gui.frameWidth - editorPos.x - buffer ), gameInfoSize.height);
-    //region editor
-    RoundedPanel editor = new RoundedPanel();
-    editor.setLocation((int) editorPos.x, (int) editorPos.y);
-    editor.setSize(editorSize);
-    editor.setArc(widgetArc);
-    editor.setBorderWidth(lineWeight);
-    editor.useColorSet(Style.normal);
-    editor.setLayout(null);
-    add(editor);
-    //endregion
+    return new ComponentSpec(tablePos, tableSize, table);
   }
 
-  public void fillEditor ()
+  public void refreshTable (int page)
   {
-    editors.removeAll();
-    editors.repaint();
-    if (!currentSettingGroup.getLeaves().isEmpty())
-    {
-      currentSettingGroup.getLeaves().values().stream()
-                         .sorted(Comparator.comparingInt(o -> o.ordinal))
-                         .forEach(leaf ->
-                         {
-                           SettingEditor sEditor = new SettingEditor(
-                               0.7,
-                               0.05,
-                               leaf
-                           );
-                           sEditor.setMaximumSize(new Dimension(Short.MAX_VALUE, 50));
-                           editors.add(sEditor);
-                           editors.add(Box.createRigidArea(new Dimension(0, 10)));
-                         });
-    }
-    editors.revalidate();
+    int startRow = page * entryCount;
+    String filter = "";
+    String sort = " ORDER BY " + dbFields[2] + " " + dbFields[3] + " LIMIT "+ startRow + "," + entryCount;
+
+    List<kn.uni.games.classic.pacman.screens.LeaderboardMenu.LeaderboardEntry> entries = PacmanDatabaseProvider.getEntries(dbFields[0] +".db", dbFields[1], filter, sort);
   }
-
-  //region tree
-  private PacTree.Node createTree ()
-  {
-    //create a node tree using depth first search
-    Config.Tree  tree = Config.getInstance().root;
-    PacTree.Node root = new PacTree.Node("Settings");
-    dfs(tree, root);
-
-    return root;
-  }
-
-  private PacTree.Node dfs (Config.Tree current, PacTree.Node node)
-  {
-    //search for children sorted by ordinal
-    current.getBranches().values().stream()
-           .sorted(Comparator.comparingInt(o -> o.ordinal))
-           .forEach(branch ->
-           {
-             node.add(dfs(branch, new PacTree.Node(branch.name)));
-           });
-
-    //    IntStream.range(0, node.getChildCount())
-    //        .mapToObj(node::getChildAt)
-    //        .map(PacTree.Node.class::cast)
-    //        .forEachOrdered(child ->
-    //        {
-    //          child.parent = node;
-    //
-    //          ArrayList<String> path = new ArrayList<>();
-    //          path.add(child.toString());
-    //
-    //          PacTree.Node parent = (PacTree.Node) child.getParent();
-    //          while (parent != null)
-    //          {
-    //            path.add(parent.toString());
-    //            parent = (PacTree.Node) parent.getParent();
-    //          }
-    //          Collections.reverse(path);
-    //          child.path = path.toArray(new String[0]);
-    //          System.out.println(Arrays.toString(child.path));
-    //        });
-
-
-    return node;
-  }
-  //endregion
 
   public void enableControls ()
   {
