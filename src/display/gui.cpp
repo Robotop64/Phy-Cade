98aa464437df9c9a3a5f1acf487afc50446a3c35
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-03-08 03:00:34 +0100

    add GitQR Code, fix font / text

--- src/display/gui.cpp
+++ src/display/gui.cpp
@@ -4,8 +4,9 @@
 Gui::Handle subinit()
 {
     Font fonts[1];
-    fonts[0] = LoadFontEx("resources/fonts/Roboto-Regular.ttf", 48, 0, 400);
+    fonts[0] = LoadFontEx("resources/fonts/Roboto-Regular.ttf", 64, 0, 400);
     SetTextureFilter(fonts[0].texture, TEXTURE_FILTER_BILINEAR);
+
     ClayMan clayMan = ClayMan(GetScreenWidth(), GetScreenHeight(), Raylib_MeasureText, fonts);
     Gui::Handle handle = {&clayMan, {fonts[0]}};
 
--- src/scenes/mainmenu.cpp
+++ src/scenes/mainmenu.cpp
@@ -15,6 +15,12 @@ struct State
 };
 static State state;
 
+struct Resources
+{
+    Texture2D QR;
+};
+static Resources resources;
+
 void calcLayout(Gui::Handle &handle);
 void processInput();
 
@@ -23,11 +29,14 @@ void Scene::MainMenu()
     Log::msg("Window", "Swap to Context: Main-Menu");
 
     state = State{};
+    resources = Resources{.QR = LoadTexture("resources/GitQR-Inverted.png")};
 
     Gui::Handle handle = Gui::init();
     calcLayout(handle);
 
     Log::msg("MainMenu", "Starting Loop.");
+
+    // Clay_SetDebugModeEnabled(true);
 
     while (!WindowShouldClose() && !state.close)
     {
@@ -79,6 +88,7 @@ void processInput()
     }
 }
 
+#pragma region Styling
 const Clay_Color gray_0 = {35, 35, 35, 255};
 const Clay_Color gray_1 = {70, 70, 70, 255};
 const Clay_Color gray_2 = {105, 105, 105, 255};
@@ -88,11 +98,43 @@ const Clay_Color gray_5 = {210, 210, 210, 255};
 const Clay_Color gray_6 = {245, 245, 245, 255};
 
 const Clay_TextElementConfig infoText = {
-    // Configure text
     .textColor = {255, 255, 255, 255},
     .fontId = 0,
     .fontSize = 16,
+    .letterSpacing = 2,
 };
+const Clay_TextElementConfig buttonText = {
+    .textColor = {255, 255, 255, 255},
+    .fontId = 0,
+    .fontSize = 32,
+    .letterSpacing = 2,
+};
+const Clay_TextElementConfig titleText = {
+    .textColor = {255, 255, 255, 255},
+    .fontId = 0,
+    .fontSize = 48,
+    .letterSpacing = 2,
+};
+#pragma endregion Styling
+
+void MenuButton(std::string label, ClayMan &clayMan)
+{
+    clayMan.element(
+        {
+            // .id = CLAY_ID("Test"),
+            .layout = {
+                .sizing = clayMan.fixedSize(250, 50),
+                .padding = {8, 8, 8, 8},
+                .childAlignment = {.x = CLAY_ALIGN_X_CENTER, .y = CLAY_ALIGN_Y_CENTER},
+            },
+            .backgroundColor = gray_2,
+            .border = {.color = gray_3, .width = {5, 5, 5, 5, 5}},
+        },
+        [&]()
+        {
+            clayMan.textElement(label, buttonText);
+        });
+}
 
 void calcLayout(Gui::Handle &handle)
 {
@@ -112,7 +154,7 @@ void calcLayout(Gui::Handle &handle)
         },
         [&]()
         {
-            // Left Column
+#pragma region LeftColumn
             clayMan.element(
                 {
                     .layout = {
@@ -121,34 +163,98 @@ void calcLayout(Gui::Handle &handle)
                         .childAlignment = {.x = CLAY_ALIGN_X_LEFT, .y = CLAY_ALIGN_Y_BOTTOM},
                         .layoutDirection = CLAY_TOP_TO_BOTTOM,
                     },
-                    .backgroundColor = gray_1,
+                    // .backgroundColor = gray_1,
                 },
                 [&]()
                 {
-                    for (int i = 0; i < 3; i++)
-                    {
-                        std::string text = "Button " + std::to_string(i);
-                        clayMan.textElement(text, infoText);
-                    }
+                    clayMan.textElement("Build Version: ", infoText);
                 });
-            // Center Column
+#pragma endregion LeftColumn
+#pragma region CenterColumn
             clayMan.element(
                 {
                     .layout = {
-                        .sizing = clayMan.expandYfixedX(GetScreenWidth() * 3 / 7),
+                        .sizing = {.width = CLAY_SIZING_PERCENT(0.4), .height = CLAY_SIZING_GROW()},
+                        .childGap = 32,
+                        .childAlignment = {.x = CLAY_ALIGN_X_CENTER},
+                        .layoutDirection = CLAY_TOP_TO_BOTTOM,
                     },
-                    .backgroundColor = gray_1,
+                    // .backgroundColor = gray_1,
                 },
-                [&]() {});
-            // Right Column
+                [&]()
+                {
+                    // Buffer
+                    clayMan.element({.layout = {.sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_PERCENT(0.05)}}},
+                                    [&]() {});
+                    // Title
+                    clayMan.element(
+                        {
+                            .layout = {
+                                .sizing = clayMan.expandXfixedY(50),
+                                .childAlignment = {.x = CLAY_ALIGN_X_CENTER, .y = CLAY_ALIGN_Y_CENTER},
+                            },
+                            // .backgroundColor = gray_2,
+                        },
+                        [&]()
+                        {
+                            clayMan.textElement("PacMan: Phi-cade Edition", titleText);
+                        });
+                    // Buffer
+                    clayMan.element({.layout = {.sizing = {.width = CLAY_SIZING_GROW(), .height = CLAY_SIZING_PERCENT(0.10)}}},
+                                    [&]() {});
+                    // Buttons
+                    clayMan.element(
+                        {
+                            .layout = {
+                                .sizing = CLAY_SIZING_FIT(),
+                                .childGap = 32,
+                                .childAlignment = {.x = CLAY_ALIGN_X_CENTER, .y = CLAY_ALIGN_Y_CENTER},
+                                .layoutDirection = CLAY_TOP_TO_BOTTOM,
+                            },
+                            // .backgroundColor = gray_2,
+                        },
+                        [&]()
+                        {
+                            MenuButton("Play", clayMan);
+                            MenuButton("Leaderboard", clayMan);
+                            MenuButton("Settings", clayMan);
+                            MenuButton("Credits", clayMan);
+                        });
+                });
+#pragma endregion CenterColumn
+#pragma region RightColumn
             clayMan.element(
                 {
                     .layout = {
                         .sizing = clayMan.expandXY(),
+                        .childGap = 8,
+                        .childAlignment = {.x = CLAY_ALIGN_X_RIGHT, .y = CLAY_ALIGN_Y_BOTTOM},
+                        .layoutDirection = CLAY_TOP_TO_BOTTOM,
                     },
-                    .backgroundColor = gray_1,
+                    // .backgroundColor = gray_1,
                 },
-                [&]() {});
+                [&]()
+                {
+                    clayMan.element(
+                        {
+                            .layout = {
+                                .sizing = clayMan.fixedSize(250, 30),
+                                .childAlignment = {.x = CLAY_ALIGN_X_CENTER, .y = CLAY_ALIGN_Y_CENTER},
+                            },
+                            // .backgroundColor = gray_2,
+                        },
+                        [&]()
+                        { clayMan.textElement("Problems & Suggestions to:", infoText); });
+                    clayMan.element(
+                        {
+                            .layout = {
+                                .sizing = clayMan.fixedSize(200, 200),
+                            },
+                            .image = {.imageData = &resources.QR, .sourceDimensions = {1000, 1000}},
+                        },
+                        [&]() {});
+                });
+#pragma endregion RightColumn
         });
 
     handle.commands = clayMan.endLayout();
