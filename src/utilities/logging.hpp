dc4adad710864500d0dd77b4421157a2a8045c7c
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-03-14 15:05:34 +0100

    improve ease of utility of logging

--- src/utilities/logging.hpp
+++ src/utilities/logging.hpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <format>
+#include <string>
 
 namespace Log
 {
     template <typename... Args>
-    void msg(const char *region, std::format_string<Args...> fmt, Args &&...args)
+    void msg(const std::string region, std::format_string<Args...> fmt, Args &&...args)
     {
         std::cout << region << " => " << std::format(fmt, std::forward<Args>(args)...) << "\n";
     }
