54b55a03a204c70ad87df98d79d559397e3f7cc6
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-03-18 00:30:44 +0100

    update Node.hpp
    +m parent pointer
    +fn printTree
    *fn addChild to correctly assign parent
    +fn path

--- src/config/node.hpp
+++ src/config/node.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "logging.hpp"
+
 #include <any>
 #include <vector>
 #include <optional>
@@ -13,10 +15,11 @@ public:
         Leaf
     };
 
-    Node(std::string name, std::any value = std::nullopt)
-        : name(name), value(value), node_type(setType(value)) {}
+    Node(std::string name, std::any value = std::nullopt, Node *parent = nullptr)
+        : name(std::move(name)), value(std::move(value)), node_type(setType(value)), parent(parent) {}
 
     std::vector<Node> children;
+    Node *parent;
 
     type node_type;
     std::string name;
@@ -44,11 +47,59 @@ public:
         return value;
     };
 
-    void addChild(Node child)
+    Node *addChild(Node child)
     {
-        children.push_back(child);
         if (node_type == Leaf)
+        {
             node_type = Group;
+            // Log::msg("Node", "Converted Node: {} to Group", name);
+        }
+
+        children.push_back(std::move(child));
+        Node *new_child = &children.back();
+        new_child->parent = this;
+
+        // Log::msg("Node", "Added Child: {} with parent {}", new_child->name, new_child->parent->name);
+        return new_child;
+    };
+
+    void printTree()
+    {
+        Log::msg("Node", "Name: {}, Parent: {}, Children: {}",
+                 name,
+                 parent != nullptr ? parent->name : "undefined",
+                 [&]()
+                 {
+                     std::string children = "{";
+                     for (auto child : this->children)
+                     {
+                         children += child.name + ", ";
+                     }
+                     children += "}";
+                     return children;
+                 }());
+
+        for (auto child : children)
+        {
+            // Log::msg("Node", "Observe Child: {}", child.name);
+            if (child.node_type == Group)
+            {
+                // Log::msg("Node", "Attempt Print on Node: {}", child.name);
+                child.printTree();
+            }
+        }
+    };
+
+    std::string path()
+    {
+        std::string path = "";
+        Node *current = this;
+        while (current->parent != nullptr)
+        {
+            path = current->name + (path == "" ? "" : ".") + path;
+            current = current->parent;
+        }
+        return path;
     };
 
 private:
