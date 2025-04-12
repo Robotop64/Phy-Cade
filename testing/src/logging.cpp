c05964741671a7b61c616c858a855f52b278785a
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-04-12 21:18:44 +0200

    move WIP config

--- testing/src/logging.cpp
+++
@@ -1,6 +0,0 @@
-#include "logging.hpp"
-
-void Log::newline()
-{
-    std::cout << "\n";
-};
--- testing/src/main.cpp
+++ testing/src/main.cpp
@@ -1,15 +1,18 @@
 #include "config.hpp"
 
 #include <iostream>
+#include <typeinfo>
 
 int main(void)
 {
-    NodePtr def = Config::gen_default_config();
+    Config &config = Config::instance();
+    config.init();
 
-    Config::save_config("config.json", def);
-    
-    NodePtr par = Config::load_config("config.json");
-    par->printTree(true);
+    OptionPtr opt = config.tree()->at("display.general")->getOption("resolution");
+    OptionPtr opt2 = config.map().get("display.general-resolution").value();
+
+    std::shared_ptr<OptionChoice<std::string>> opt_choice = std::dynamic_pointer_cast<OptionChoice<std::string>>(opt);
+    std::shared_ptr<OptionChoice<std::string>> opt_choice2 = std::dynamic_pointer_cast<OptionChoice<std::string>>(opt2);
 
     return 0;
 };
