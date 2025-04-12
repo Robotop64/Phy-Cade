b4044c8f740f16a3322e5dc54d2eb4463f61bd62
David Capalbo <117840567+Robotop64@users.noreply.github.com>
2025-04-12 21:20:04 +0200

    move logging to utilities

--- src/utilities/logging.hpp
+++ src/utilities/logging.hpp
@@ -6,9 +6,36 @@
 
 namespace Log
 {
+    static std::string last_prefix = "";
+
     template <typename... Args>
     void msg(const std::string region, std::format_string<Args...> fmt, Args &&...args)
     {
-        std::cout << region << " => " << std::format(fmt, std::forward<Args>(args)...) << "\n";
+        std::string out = region + " => " + std::format(fmt, std::forward<Args>(args)...);
+        std::cout << out << "\n";
+
+        last_prefix = region;
     }
+
+    template <typename... Args>
+    void updated(const std::string region, std::format_string<Args...> fmt, Args &&...args, bool overwrite = false)
+    {
+        static int last_size = 0;
+        std::cout << "\r" << std::string(last_size, ' ') << "\r";
+
+        std::string out = region + " => " + std::format(fmt, std::forward<Args>(args)...);
+        last_size = out.size();
+        std::cout << out << std::flush;
+
+        last_prefix = region;
+    }
+
+    template <typename... Args>
+    void bullet(std::format_string<Args...> fmt, Args &&...args)
+    {
+        std::string out = std::string(last_prefix.size(), ' ') + " => " + std::format(fmt, std::forward<Args>(args)...);
+        std::cout << out << "\n";
+    }
+
+    void newline();
 }
